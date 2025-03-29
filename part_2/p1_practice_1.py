4e654858fc397a2fb64177f8af6dfebcfe4af3bc
Valentin <bystrich@gmail.com>
2025-03-29 09:30:18 +0100

    Initial Commit

---
+++ part_2/p1_practice_1.py
@@ -0,0 +1,18 @@
+# Warum funktioniert den Code nicht? Versuche ihn zu fixen.
+neue_nummer = int(input("Bitte geben sie eine nummer ein: "))
+
+if neue_nummer > 0:
+    if neue_nummer % 2 == 0:
+        print("Die Nummer ist gerade")
+else:
+    print("Die Nummer ist ungerade")
+else:
+print("Die Nummer ist null oder negativ")
+
+# Schreibe Code der nach dem Alter des Benutzers fragt und durch EINE If-Anweisung überprüft ob das genannte Alter sinnvoll ist (z.B. zwischen 5 und 100 Jahren)
+
+
+# Ändere den Code so, dass die If-Anweisung wahr ist, wenn die Zahl nicht zwischen 5 und 8 liegt. Tip: dafür muss nur ein Wort hinzugefügt werden!
+number = int(input("Bitte geben sie eine nummer ein: "))
+if (number >= 5 and number <= 8):
+    print("Die eingebene nummer ist nicht zwischen 5 und 8")
---
+++ projects/project_1.py
@@ -0,0 +1,3 @@
+# Schreibe ein Programm, mit dem du zwei (ganze) Zahlen addieren, multiplizieren, subtrahieren oder
+# dividieren kannst. Schreibe das Programm so, dass nach dem Ausgeben des Ergebnisses gefragt wird,
+# ob eine weitere Rechnung gemacht werden soll!
