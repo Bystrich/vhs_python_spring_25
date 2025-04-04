6a9fe1e71a72c361ca5058bfd988e20b8f70b1b5
Valentin <bystrich@gmail.com>
2025-04-04 17:15:37 +0200

    Minor fix.

--- part_3/p3_examples.py
+++
@@ -1,7 +0,0 @@
-# Definiere eine rekursive Funktion die n-te Fibonacci Nummer ausgibt (schwer)
-
-def fibonacci(n):
-    return 1
-
-
-print(fibonacci(4))  # sollte 3 ausgeben (denn die sequence ist 0, 1, 1, 2, 3, 5, 8 usw.)
