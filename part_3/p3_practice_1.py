03c344fce6b3c3f0d8253142132c41b31f0264c8
Valentin <bystrich@gmail.com>
2025-04-04 17:09:04 +0200

    Prepare second session.

---
+++ part_3/p3_practice_1.py
@@ -0,0 +1,8 @@
+# Schreibe eine Funktion die bis zu einer beliebigen ganzen Zahl (n) hochzählt und dann einen String n-Mal ausgibt.
+
+
+# Schreibe eine Funktion die überprüft ob zwei bestimmte Buchstaben in einem Wort gleich sind.
+# So sollte z.B. print(gleiche_buchstaben("programmierer", 6, 7)) True ausgeben
+
+def gleiche_buchstaben(wort, b_1, b_2):
+    return False
---
+++ part_3/p3_practice_3.py
@@ -0,0 +1,3 @@
+# Schreibe ein kleines Programm, dass den Nutzer nach einer Nummer fragt und dann alle nummern von dem negativen der
+# Zahl bis zur Zahl einschließlich aufzählt. Dafür sollen jeweils neue Zeilen genutzt werden. Z.B. für n = 10: -10,
+# -9, -8,......6, 7, 8, 9, 10
