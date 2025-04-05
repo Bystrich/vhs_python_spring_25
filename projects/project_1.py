13d72f48b24ad6287cb9e326bc494d96985d05e1
Valentin <valentin.bystrich@nfc21.de>
2025-04-05 10:34:49 +0200

    Adds square root function.

--- projects/project_1.py
+++ projects/project_1.py
@@ -1,3 +1,17 @@
 # Schreibe ein Programm, mit dem du zwei (ganze) Zahlen addieren, multiplizieren, subtrahieren oder
 # dividieren kannst. Schreibe das Programm so, dass nach dem Ausgeben des Ergebnisses gefragt wird,
 # ob eine weitere Rechnung gemacht werden soll!
+
+import math
+
+
+def square_root(x):
+    if x < 0:
+        return "Es kann nicht die wurzel von einer Zahl kleiner als 0 gezogen werden!"
+    else:
+        return math.sqrt(x)
+
+print(square_root(3))
+
+
+
