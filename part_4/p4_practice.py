1b067fac719d45151949886dd003ca0184ae4d80
Valentin <valentin.bystrich@nfc21.de>
2025-04-05 09:22:45 +0200

    Adds part 4.

---
+++ part_4/p4_practice.py
@@ -0,0 +1,7 @@
+# Erstelle eine Klasse "Book" mit den Attributen: title, author, total_pages, pages_read
+# Mit der Methode read mit, die eine beliebige Anzahl Seiten zu pages_read hinzufügt
+# Und mit der Methode progress die den Fortschritt in Prozent ausgibt
+
+# Erstelle eine Klasse "Audiobook" die von Book erbt.
+# Zusätzlich soll die Klasse ein Attribut narrator haben
+# Alle Methoden sollen sinnvoll angepasst werden
---
+++ projects/project_2.py
@@ -0,0 +1,2 @@
+# Baue erneut den Taschenrechner. Diesmal aber als Klasse "Calculator".
+# Der Nutzer soll sich zu Beginn mit einem Passwort anmelden!
